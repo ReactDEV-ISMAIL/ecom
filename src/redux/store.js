f0239b9d0d2305629de14cf8ea79a990c9b179cd
ReactDEV-ISMAIL <bouziane.ismail16@gmail.com>
2020-10-13 21:48:24 +0100

    removing logger from prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,10 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const midlwares = [logger];
+const midlwares = [];
+if (process.env.NODE_ENV === "development") {
+  midlwares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...midlwares));
 export const persistor = persistStore(store);
